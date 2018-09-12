a847c393039b394b380c0fab94f9995cff184e5c
Luke Hammer <textchimp@gmail.com>
2018-09-12 17:18:08 +1000

    loops and arrays exercise solutions

---
+++ week1/arrays-exercises/js/main.js
@@ -0,0 +1,23 @@
+
+console.log('JS loaded!');
+
+// Your top choices
+//
+// Create an array to hold your top choices (colors, presidents, whatever).
+// For each choice, log to the screen a string like: "My #1 choice is blue."
+// Bonus: Change it to log "My 1st choice, "My 2nd choice", "My 3rd choice", picking the right suffix for the number based on what it is.
+//
+
+const topDogs = [
+  'Novia Scotia Duck-Tolling Retriever',
+  'Leonberger',
+  'Husky',
+  'Golden Retriever',
+  'Chilean Terrier'
+];
+
+const suffixes = ['st', 'nd', 'rd', 'th', 'th'];
+
+for( let i = 0; i < topDogs.length; i++ ){
+  console.log(`My ${ (i+1) + suffixes[i] } choice is: ${topDogs[i]}`);
+}
---
+++ week1/loops-exercises/js/main.js
@@ -0,0 +1,108 @@
+
+console.log('JS loaded!');
+
+
+// Exercises: For loops
+// The even/odd reporter
+// Write a for loop that will iterate from 0 to 20. For each iteration, it will check if the current number is even or odd, and report that to the screen (e.g. "2 is even").
+
+for( let i = 0; i <= 20; i++ ){
+  // if( i % 2 === 0 ){
+  //   console.log(`${i} is even.`);
+  // } else {
+  //   console.log(`${i} is odd.`);
+  // }
+
+  let status = 'odd';
+  if( i % 2 === 0 ){
+    status = 'even';
+  }
+
+  console.log(`${i} is ${status}`);
+
+} // for
+
+// console.log(`Value of i is ${i}`);  // throws an error
+
+
+// Multiplication Tables
+// Write a for loop that will iterate from 0 to 10. For each iteration of the for loop, it will multiply the number by 9 and log the result (e.g. "2 * 9 = 18").
+//
+// Bonus: Use a nested for loop to show the tables for every multiplier from 1 to 10 (100 results total).
+
+for( let i = 0; i < 10; i++ ){
+  for( let j = 0; j < 10; j++ ){
+    const result = i * j;
+    if( result % 10 === 0 ){
+      console.log(`%c ${i} * ${j} = ${ result }`, 'color: hotpink; font-size: 16pt;');
+    } else {
+      console.log(`${i} * ${j} = ${ result }`);
+    }
+  }
+} // for
+
+
+// The Grade Assigner
+// Check the results of assignGrade function from the conditionals exercise for every value from 60 to 100 - so your log should show "For 89, you got a B. For 90, you got an A.", etc.
+
+// The Grade Assigner
+// Write a function named assignGrade that:
+//
+// takes 1 argument, a number score.
+// returns a grade for the score, either "A", "B", "C", "D", or "F" (for example, scores above 90 receive an "A", scores between 90 and 80 receive a "B", and so on).
+// Call that function for a few different scores and log the result to make sure it works.
+
+const assignGrade = function( score ){
+  // let grade = '';
+  // if( score >= 90 ){
+  //   grade = 'A';
+  // } else if( score >= 80 ){
+  //   grade = 'B';
+  // } else if( score >= 70 ){
+  //   grade = 'C';
+  // } else if( score >= 60 ){
+  //   grade = 'D'
+  // } else {
+  //   grade = 'F';
+  // }
+  // let grade = '';
+
+  if( score >= 90 ){
+    return 'A';
+  } else if( score >= 80 ){
+    return 'B';
+  } else if( score >= 70 ){
+    return 'C';
+  } else if( score >= 60 ){
+    return 'D'
+  } else {
+    return 'F';
+  }
+
+
+  // if( score >= 90 ){
+  //   return 'A';
+  // }
+  // if( score >= 80 ){
+  //   return 'B';
+  // }
+  // if( score >= 70 ){
+  //   return 'C';
+  // }
+  // if( score >= 60 ){
+  //   return 'D'
+  // }
+  return 'F';
+
+  // return grade;
+};
+
+console.log(assignGrade(95));
+console.log(assignGrade(80));
+console.log(assignGrade(72));
+console.log(assignGrade(60));
+console.log(assignGrade(40));
+
+for( let i = 60; i <= 100; i++ ){
+  console.log(`For the score ${i}, you get the grade: ${ assignGrade(i) }`);
+}
